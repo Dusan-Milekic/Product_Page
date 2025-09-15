0238ae6ff0ded04ef3e5dd1b59fcf8c6887b9049
Dusan-Milekic <dusanmilekic0511@gmail.com>
2025-09-15 15:43:15 +0200

    nothignb

--- product-app/src/Header.tsx
+++ product-app/src/Header.tsx
@@ -3,11 +3,12 @@ import cart from "./assets/icon-cart.svg";
 import avatar from "./assets/image-avatar.png";
 import sneakers from "./assets/sneakers 2.svg";
 import "./Header.css";
+
 export default function Header() {
   return (
     <>
       <div className="header flex justify-between max-h-10  px-8">
-        <div className="left-side flex gap-8">
+        <div className="left-side flex gap-8 ">
           <div id="menu" className="img-holder flex items-center">
             <img src={ico_menu} alt="icon_menu" />
           </div>
